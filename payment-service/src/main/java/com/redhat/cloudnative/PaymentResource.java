39bf86c2a279544d51f24e44ce1df9929c8280c5
admin <admin@admin.com>
2020-09-15 02:14:35 +0000

    updated

--- payment-service/src/main/java/com/redhat/cloudnative/PaymentResource.java
+++ payment-service/src/main/java/com/redhat/cloudnative/PaymentResource.java
@@ -27,15 +27,92 @@ import io.vertx.core.json.JsonObject;
 public class PaymentResource {
 
     // TODO: Add Messaging ConfigProperty here
+    @ConfigProperty(name = "mp.messaging.outgoing.payments.bootstrap.servers")
+    public String bootstrapServers;
+
+    @ConfigProperty(name = "mp.messaging.outgoing.payments.topic")
+    public String paymentsTopic;
+
+    @ConfigProperty(name = "mp.messaging.outgoing.payments.value.serializer")
+    public String paymentsTopicValueSerializer;
+
+    @ConfigProperty(name = "mp.messaging.outgoing.payments.key.serializer")
+    public String paymentsTopicKeySerializer;
+
+    private Producer<String, String> producer;
+
+    public static final Logger log = LoggerFactory.getLogger(PaymentResource.class);
 
     // TODO: Add handleCloudEvent method here
 
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void handleCloudEvent(String cloudEventJson) {
+        String orderId = "unknown";
+        String paymentId = "" + ((int)(Math.floor(Math.random() * 100000)));
+
+        try {
+            log.info("received event: " + cloudEventJson);
+            JsonObject event = new JsonObject(cloudEventJson);
+            orderId = event.getString("orderId");
+            String total = event.getString("total");
+            JsonObject ccDetails = event.getJsonObject("creditCard");
+            String name = event.getString("name");
+
+            // fake processing time
+            Thread.sleep(5000); 
+            if (!ccDetails.getString("number").startsWith("4")) {
+                fail(orderId, paymentId, "Invalid Credit Card: " + ccDetails.getString("number"));
+            } else {
+                pass(orderId, paymentId, "Payment of " + total + " succeeded for " + name + " CC details: " + ccDetails.toString());
+            }
+        } catch (Exception ex) {
+             fail(orderId, paymentId, "Unknown error: " + ex.getMessage() + " for payment: " + cloudEventJson);
+        }
+    }
+
     // TODO: Add pass method here
+    private void pass(String orderId, String paymentId, String remarks) {
+
+        JsonObject payload = new JsonObject();
+        payload.put("orderId", orderId);
+        payload.put("paymentId", paymentId);
+        payload.put("remarks", remarks);
+        payload.put("status", "COMPLETED");
+        log.info("Sending payment success: " + payload.toString());
+        producer.send(new ProducerRecord<String, String>(paymentsTopic, payload.toString()));
+    }
 
     // TODO: Add fail method here
+    private void fail(String orderId, String paymentId, String remarks) {
+        JsonObject payload = new JsonObject();
+        payload.put("orderId", orderId);
+        payload.put("paymentId", paymentId);
+        payload.put("remarks", remarks);
+        payload.put("status", "FAILED");
+        log.info("Sending payment failure: " + payload.toString());
+        producer.send(new ProducerRecord<String, String>(paymentsTopic, payload.toString()));
+    }
 
     // TODO: Add consumer method here
+    @Incoming("orders")
+    public CompletionStage<Void> onMessage(KafkaMessage<String, String> message)
+            throws IOException {
+
+        log.info("Kafka message with value = {} arrived", message.getPayload());
+        handleCloudEvent(message.getPayload());
+        return message.ack();
+    }
 
     // TODO: Add init method here
+    public void init(@Observes StartupEvent ev) {
+        Properties props = new Properties();
+
+        props.put("bootstrap.servers", bootstrapServers);
+        props.put("value.serializer", paymentsTopicValueSerializer);
+        props.put("key.serializer", paymentsTopicKeySerializer);
+        producer = new KafkaProducer<String, String>(props);
+    }
 
 }
